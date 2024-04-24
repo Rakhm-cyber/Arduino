6c805a18696988c291d38e55a035c2e655415412
Vitalya <vitamin11182005@gmail.com>
2024-04-24 18:48:19 +0300

    e

--- sketch_apr17a.ino
+++ sketch_apr17a.ino
@@ -1,29 +1,31 @@
 #include <math.h>
+#include <stdio.h>
 
 const int dirPin1 = 3;
 const int dirPin2 = 5;
 const int stepPin1 = 2;
 const int stepPin2 = 4;
-const int stepsPerRevolution = 200;
+const int stepsPerRevolution = 360;
  
 void spin()
 {
-  digitalWrite(dirPin1, HIGH); // Установка вращения по часовой стрелки
-  digitalWrite(dirPin2, HIGH);
-
-  for(int x = 0; x < stepsPerRevolution; x++)
+  digitalWrite(dirPin1, HIGH); 
+  // digitalWrite(dirPin2, HIGH);
+  digitalWrite(stepPin1, HIGH);
+  delayMicroseconds(2000);
+  digitalWrite(stepPin1, LOW);
+  //for(int x = 0; x < stepsPerRevolution; x++)
   {
-    digitalWrite(stepPin1, HIGH);
-    digitalWrite(stepPin2, HIGH);
-    delayMicroseconds(2000);
-    digitalWrite(stepPin1, LOW);
-    digitalWrite(stepPin2, LOW);
-    delayMicroseconds(2000);
+    //digitalWrite(stepPin1, HIGH);
+    //digitalWrite(stepPin2, HIGH);
+    //delayMicroseconds(2000);
+    //digitalWrite(stepPin1, LOW);
+    //digitalWrite(stepPin2, LOW);
+    //delayMicroseconds(2000);
   }
-
 }
 
-void rotate(float x, y, z)
+void rotate(float x, float y, float z)
 {
   float radius = sqrt(x*x + y*y + z*z);
   float polar = (acos(z/radius) * 180) / M_PI;
@@ -37,33 +39,29 @@ void setup()
   pinMode(stepPin2, OUTPUT);
   pinMode(dirPin2, OUTPUT);
 
-  FILE *file = fopen("jcode.txt", "r");
-
   float x;
   float y;
   float z;
   float g;
   int c; 
 
-  if (file == NULL)
-  {
-      perror("Error opening file");
-      return 1;
-  }
   char line[100];
-  while(!feof(file)){
-      fgets(line, sizeof(line), file);
-      sscanf(line, "G%d ", &c);
-      if (c == 1){
-          sscanf(line, "G1 X%f Y%f Z%f", &x, &y, &z);
-      }
-      
-  }
-  rotate(x, y, z);
-
+  //while(!feof(file))
+  //{
+  //    fgets(line, sizeof(line), stdin);
+  //    sscanf(line, "G%d ", &c);
+  //    if (c == 1)
+  //    {
+  //        sscanf(line, "G1 X%f Y%f Z%f", &x, &y, &z);
+  //    }
+  //}
+  //rotate(x, y, z);
+  for (int i = 0; i < 200; i++)
+    spin();
 }
 
 void loop()
 {
+ 
 
 }
